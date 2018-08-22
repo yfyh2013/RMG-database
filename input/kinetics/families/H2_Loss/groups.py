5d0c03c7d31c142984d789e477e804d9bac21ca7
Agnes <ajocher@mit.edu>
2018-08-22 17:39:14 -0400

    Updated name for the reverse reaction and excluding R atoms in H2_Loss groups.py.

--- input/kinetics/families/H2_Loss/groups.py
+++ input/kinetics/families/H2_Loss/groups.py
@@ -9,7 +9,7 @@ longDesc = u"""
 
 template(reactants=["Di-H-ene"], products=["ene1","H2"], ownReverse=False)
 
-reverse = "none"
+reverse = "h_addition_ene"
 
 reversible = True
 recipe(actions=[
@@ -26,20 +26,14 @@ entry(
     label = "Di-H-ene",
     group = 
 """
-1 *1 C u0 p0 c0 {2,D} {6,S} {7,S}
-2 *2 C u0 p0 c0 {1,D} {3,S} {8,S}
-3 *3 C u0 p0 c0 {2,S} {4,S} {9,S} {10,S}
-4 *4 C u0 p0 c0 {3,S} {5,D} {11,S}
-5 *5 C u0 p0 c0 {4,D} {6,S} {12,S}
-6 *6 C u0 p0 c0 {1,S} {5,S} {13,S} {14,S}
-7 R u0 p0 c0 {1,S}
-8 R u0 p0 c0 {2,S}
-9 R u0 p0 c0 {3,S}
-10 *8 H u0 p0 c0 {3,S}
-11 R u0 p0 c0 {4,S}
-12 R u0 p0 c0 {5,S}
-13 *7 H u0 p0 c0 {6,S}
-14 R u0 p0 c0 {6,S}
+1 *1 C u0 p0 c0 {2,D} {6,S}
+2 *2 C u0 p0 c0 {1,D} {3,S}
+3 *3 C u0 p0 c0 {2,S} {4,S} {8,S}
+4 *4 C u0 p0 c0 {3,S} {5,D}
+5 *5 C u0 p0 c0 {4,D} {6,S}
+6 *6 C u0 p0 c0 {1,S} {5,S} {7,S}
+7 *7 H u0 p0 c0 {6,S}
+8 *8 H u0 p0 c0 {3,S}
 """,
     kinetics = None,
 )
